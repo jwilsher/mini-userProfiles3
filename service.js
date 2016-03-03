f6f07a0c6b04acf103cc485fa851f45aa4031ffa
Jaclyn Cowley <jaclyncowley11@Jaclyns-MacBook-Pro-2.local>
2016-03-02 21:35:21 -0700

    cool stuff

--- service.js
+++ service.js
@@ -1,12 +1,17 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http) {
+app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    //var deferred = $q.defer();
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response){
+        //deferred.resolve(response.data.data);
+        return response.data.data;
     })
+    //return deferred.promise;
   }
 
 });
